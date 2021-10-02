af928532630728d6e238b82f77d1702659dfc02d
✓xAlbertx✓ <albertozaaz760@gmail.com>
2021-10-02 15:13:00 +0200

    Add files via upload

--- webserver_script.py
+++ webserver_script.py
@@ -1,152 +1,144 @@
+#!C:\Users\Ученик\AppData\Local\Programs\Python\Python39\python.exe
+import cgi
+import cgitb
+cgitb.enable()
 sl = [{
-    'name': 'Мопс',
+    'name': 'Mops',
     'height': '27-36',
-    "ves" : "6.3-8.1",
-    "naznachenie" : "Дом",
-    "Sherst" : "Глад",
-    "Agrassive" : "2",
+    "ves": "6-8",
+    "naznachenie": "Дом",
+    "Sherst": "Глад",
 },
-{
-    'name': 'Ретривер',
-    'height': '52-61',
-    "ves" : "26-40",
-    "naznachenie" : "Охота",
-    "Sherst" : "Плот Вод" , 
-    "Agrassive" : "1" ,
-},
-{
-    'name': 'Рассер терьер',
-    'height': '22-30',
-    "ves" : "5-6",
-    "naznachenie" : "Охота",
-    "Sherst" : "Глад",
-    "Agrassive" : "4",
-},
-{
+    {
+        'name': 'Retryver',
+        'height': '52-61',
+        "ves": "26-40",
+        "naznachenie": "Охот",
+        "Sherst": "Вод",
+    },
+    {
+        'name': 'Rasser Teryer',
+        'height': '22-30',
+        "ves": "5-6",
+        "naznachenie": "Охот",
+        "Sherst": "Глад",
+    },
+    {
 
-    'name': 'Немецкая овчарка',
-    'height': '55-65',
-    "ves" : "22-40",
-    "naznachenie" : "Служ",
-    "Sherst" : "Плот",
-    "Agrassive" : "3",
-},
-{
-    'name': 'Английский бульдог',
-    'height': '50-55',
-    "ves" : "24",
-    "naznachenie" : "Дом",
-    "Sherst" : "Глад",
-    "Agrassive" : "2",
-},
-{
-    'name': 'Акита-ину',
-    'height': '60-70',
-    "ves" : "20-40",
-    "naznachenie" : "Охот",
-    "Sherst" : "Плот",
-    "Agrassive" : "2",
-},
-{
-    'name': 'Сербернар',
-    'height': '65-85',
-    "ves" : "70-85",
-    "naznachenie" : "Служ",
-    "Sherst" : "Плот",
-    "Agrassive" : "2",
-},
-{
-    'name': 'Хаски',
-    'height': '50-60',
-    "ves" : "15-28",
-    "naznachenie" : "Служ",
-    "Sherst" : "Плот",
-    "Agrassive" : "5",
-},
-{
-    'name': 'Бигль',
-    'height': '33-40',
-    "ves" : "9-14",
-    "naznachenie" : "Охота",
-    "Sherst" : "Глад",
-    "Agrassive" : "1",
-},
-{
-    'name': 'Шпиц',
-    'height': '40-50',
-    "ves" : "17-22",
-    "naznachenie" : "Дом",
-    "Sherst" : "Плот",
-    "Agrassive" : "3",
-},
-{
-    'name': 'Доберман',
-    'height': '63-72',
-    "ves" : "32-45",
-    "naznachenie" : "Служ",
-    "Sherst" : "Корот",
-    "Agrassive" : "3",
-},
-{
-    'name': 'Бультерьер',
-    'height': '47-56',
-    "ves" : "23-38",
-    "naznachenie" : "Дом",
-    "Sherst" : "Глад",
-    "Agrassive" : "5",
-},
-{
-    'name': 'Далматинец',
-    'height': '54-62',
-    "ves" : "24-32",
-    "naznachenie" : "Охот дом",
-    "Sherst" : "Корот",
-    "Agrassive" : "4",
-},
-{
-    'name': 'Ротвейлер',
-    'height': '56-68',
-    "ves" : "38-53",
-    "naznachenie" : "Служ",
-    "Sherst" : "Глад",
-    "Agrassive" : "5",
-},
-{
-    'name': 'Cиба-ину',
-    'height': '35-41', 
-    "ves" : "7-13",
-    "naznachenie" : "Охот",
-    "Sherst" : "Плот",
-    "Agrassive" : "6",
-},
-{
-    'name': 'Ньюфаундленд',
-    'height': '61-76', 
-    "ves" : "49-53",
-    "naznachenie" : "Служ",
-    "Sherst" : "Плот вод",
-    "Agrassive" : "2",
-}
+        'name': 'Nemeckaya Ovcharka',
+        'height': '55-65',
+        "ves": "22-40",
+        "naznachenie": "Служ",
+        "Sherst": "Плот",
+    },
+    {
+        'name': 'Angliyskii Buldog',
+        'height': '50-55',
+        "ves": "24-25",
+        "naznachenie": "Дом",
+        "Sherst": "Глад",
+    },
+    {
+        'name': 'Akyta-Inu',
+        'height': '60-70',
+        "ves": "20-40",
+        "naznachenie": "Охот",
+        "Sherst": "Плот",
+    },
+    {
+        'name': 'Serbynar',
+        'height': '65-85',
+        "ves": "70-85",
+        "naznachenie": "Служ",
+        "Sherst": "Плот",
+    },
+    {
+        'name': 'Hasky',
+        'height': '50-60',
+        "ves": "15-28",
+        "naznachenie": "Служ",
+        "Sherst": "Плот",
+    },
+    {
+        'name': 'Bygl',
+        'height': '33-40',
+        "ves": "9-14",
+        "naznachenie": "Охот",
+        "Sherst": "Глад",
+    },
+    {
+        'name': 'Shpyc',
+        'height': '40-50',
+        "ves": "17-22",
+        "naznachenie": "Дом",
+        "Sherst": "Плот",
+    },
+    {
+        'name': 'Doberman',
+        'height': '63-72',
+        "ves": "32-45",
+        "naznachenie": "Служ",
+        "Sherst": "Корот",
+    },
+    {
+        'name': 'Bulteryer',
+        'height': '47-56',
+        "ves": "23-38",
+        "naznachenie": "Дом",
+        "Sherst": "Глад",
+    },
+    {
+        'name': 'Dalmatyn',
+        'height': '54-62',
+        "ves": "24-32",
+        "naznachenie": "Дом",
+        "Sherst": "Корот",
+    },
+    {
+        'name': 'Rotveyler',
+        'height': '56-68',
+        "ves": "38-53",
+        "naznachenie": "Служ",
+        "Sherst": "Глад",
+    },
+    {
+        'name': 'Syba-Inu',
+        'height': '35-41',
+        "ves": "7-13",
+        "naznachenie": "Охот",
+        "Sherst": "Плот",
+    },
+    {
+        'name': 'Newfundland',
+        'height': '61-76',
+        "ves": "49-53",
+        "naznachenie": "Служ",
+        "Sherst": "Вод",
+    }
 ]
+form = cgi.FieldStorage()
+height_dog = form.getfirst("INT_1", "int")
+weight_dog = form.getfirst("INT_2", "int")
+nazn_dog = form.getfirst("TEXT_1", "не задано")
+sherst_dog = form.getfirst("TEXT_2", "не задано")
 shake_y = 5
-shake_xz = 3
-height_dog = int(input('Высота собаки: ')) #изменить на входные данные с qt
-weight_dog = int(input('Вес собаки: ')) #изменить на входные данные с qt
-nazn_dog = str(input('Назначение собаки: ')) #изменить на входные данные с qt
-sherst_dog = str(input('Шерсть собаки: ')) #изменить на входные данные с qt
+shake_xz = 4
 
-def find_nearbest(in_dic:dict, sl_dic:dict):
+
+def find_nearbest(in_dic: dict, sl_dic: dict):
     nearbest = []
     prom = []
 
     for i in sl_dic:
         for j in sl_dic:
-            prom.append([i['name'] + '+' + j['name'], 0])
+            prom.append([i['name'] + ' + ' + j['name'], 0])
 
-            if round((sr(i['height']) + sr(j['height'])) / 2) >= int(in_dic['height']) - 5 and round((sr(i['height']) + sr(j['height'])) / 2) <= int(in_dic['height']) + shake_y:
+            if round((sr(i['height']) + sr(j['height'])) / 2) >= int(in_dic['height']) - 5 and round(
+                    (sr(i['height']) + sr(j['height'])) / 2) <= int(in_dic['height']) + shake_y:
                 prom[-1][1] += 3
 
-            if round((sr(i['ves']) + sr(j['ves'])) / 2) >= int(in_dic['ves']) - 3 and round((sr(i['ves']) + sr(j['ves'])) / 2) <= int(in_dic['ves']) + shake_y:
+            if round((sr(i['ves']) + sr(j['ves'])) / 2) >= int(in_dic['ves']) - 3 and round(
+                    (sr(i['ves']) + sr(j['ves'])) / 2) <= int(in_dic['ves']) + shake_y:
                 prom[-1][1] += 1
 
             if i['naznachenie'] == j['naznachenie'] == in_dic['naznachenie']:
@@ -158,7 +150,7 @@ def find_nearbest(in_dic:dict, sl_dic:dict):
                 prom[-1][1] += 2
             elif i['Sherst'] == in_dic['Sherst'] or j['Sherst'] == in_dic['Sherst']:
                 prom[-1][1] += 1
-        
+
         inp = prom[0]
         for k in range(len(prom)):
             if inp[1] < prom[k][1]:
@@ -167,20 +159,69 @@ def find_nearbest(in_dic:dict, sl_dic:dict):
         prom = []
     inp2 = nearbest[0]
     for k in range(len(nearbest)):
-            if inp2[1] < nearbest[k][1]:
-                inp2 = nearbest[k]
+        if inp2[1] < nearbest[k][1]:
+            inp2 = nearbest[k]
     return inp2
+
 
 def sr(st):
     lst = st.split('-')
-    ret = (int(lst[0]) + int(lst[1]))/2
+    ret = (int(lst[0]) + int(lst[1])) / 2
     return ret
 
+
 in_sl = {
-    'height': height_dog, 
-    "ves" : weight_dog,
-    "naznachenie" : nazn_dog,
-    "Sherst" :  sherst_dog,
+    'height': height_dog,
+    "ves": weight_dog,
+    "naznachenie": nazn_dog,
+    "Sherst": sherst_dog,
 }
 
-print(find_nearbest(in_sl, sl))
+print("Content-type: text/html\n")
+print("""<!DOCTYPE HTML>
+        <html lang="ru">
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Обработка данных форм</title>
+            <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	        <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	        <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	        <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	        <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	        <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	        <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	        <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	        <link rel="stylesheet" type="text/css" href="css/util.css">
+	        <link rel="stylesheet" type="text/css" href="css/main.css">
+	        <script src ="vendor/jquery/jquery-3.2.1.min.js"></script>
+            <script src ="vendor/animsition/js/animsition.min.js"></script>
+            <script src ="vendor/bootstrap/js/popper.js"></script>
+            <script src ="vendor/bootstrap/js/bootstrap.min.js"></script>
+            <script src ="vendor/select2/select2.min.js"></script>
+            <script src ="vendor/daterangepicker/moment.min.js"></script>
+            <script src ="vendor/daterangepicker/daterangepicker.js"></script>
+            <script src ="vendor/countdowntime/countdowntime.js"></script>
+            <script src ="js/main.js"></script>
+        </head>
+        <body>""")
+pre_result = find_nearbest(in_sl, sl)
+result = pre_result[0].split("+")
+resul_1 = result[0]
+resul_2 = result[1]
+sovmes = pre_result[1]
+print("""<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-t-1500 p-b-1200">
+				<form action="webserver_script.py" class="login100-form validate-form flex-sb flex-w">
+					<span class="login100-form-title p-b-51">
+						<h1>Genetic Code Manipulator</h1>
+					</span>""")
+print("<h1>{}<h1>".format(pre_result))
+print("""	</div>
+		</div>
+	</div>""")
+
+print("""</body>
+        </html>""")
